c65a57c4b4c29739e7e5a6f1b82b08889d049616
Roger Lai <roger.lai.03@gmail.com>
2017-07-29 19:26:49 -0700

    Adding all code from tutorial

---
+++ django-polls/polls/urls.py
@@ -0,0 +1,29 @@
+from django.conf.urls import url
+
+from . import views
+
+app_name = 'polls'
+urlpatterns = [
+    url(r'^$', views.IndexView.as_view(), name='index'),
+    url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
+    url(r'^(?P<pk>[0-9]+)/results/$', views.ResultsView.as_view(), name='results'),
+    url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
+]
+
+
+# from django.conf.urls import url
+# from . import views
+
+# app_name = 'polls'
+# urlpatterns = [
+#     # ex: /polls/
+#     url(r'^$', views.index, name='index'),
+#     # ex: /polls/5/
+#     url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
+#     # Using parentheses around a pattern “captures” the text matched by that pattern and sends it as an argument to the view function
+#     # ?P<question_id> defines the name that will be used to identify the matched pattern
+#     # ex: /polls/5/results/
+#     url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
+#     # ex: /polls/5/vote/
+#     url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
+# ]
